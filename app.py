1113127f3c182d07acdfbb0ad5d0f1e0db49c2c7
JericHunter <jeric.hunterr@gmail.com>
2019-11-06 03:39:18 -0800

    worked a ton

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template, redirect, url_for, request
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    """Return homepage."""
+    return render_template('home.html')
+
+@app.route('/gallery')
+def gallery():
+    """Return homepage."""
+    return render_template('gallery.html')
