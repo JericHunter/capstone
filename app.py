a50f12ab13c5dea8fbc788d344d09cf1cc0de6fc
JericHunter <jeric.hunterr@gmail.com>
2019-11-06 03:57:12 -0800

    worked a lil fix

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     """Return homepage."""
-    return render_template('home.html')
+    return render_template('index.html')
 
 @app.route('/gallery')
 def gallery():
